fb1cd22447eaadfd4d9754a1b989b60093b6dd41
AveJaPl <filip.piatek2005@gmail.com>
2024-10-14 22:31:31 +0200

    main layout created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { Header } from "./components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Welcome to o-talk</h1>
+        <p className="text-gray-500">A place to talk about anything you want</p>
+      </main>
+      <footer className="p-4 text-center text-gray-500">
+        <p>© 2021 o-talk</p>
+      </footer>
     </div>
   );
 }
