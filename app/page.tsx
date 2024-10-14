4f7b5b7b77563d7f4c2b9717ff35126ac658eb5f
AveJaPl <filip.piatek2005@gmail.com>
2024-10-14 23:26:49 +0200

    General layout adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import { Header } from "./components/Header";
-
 export default function Home() {
   return (
     <div>
-      <Header />
-      <main className="p-4">
-        <h1 className="text-2xl font-bold">Welcome to o-talk</h1>
-        <p className="text-gray-500">A place to talk about anything you want</p>
-      </main>
-      <footer className="p-4 text-center text-gray-500">
-        <p>© 2021 o-talk</p>
-      </footer>
+      <h1 className="text-2xl font-bold">Welcome to o-talk</h1>
+      <p className="text-gray-500">A place to talk about anything you want</p>
     </div>
   );
 }
